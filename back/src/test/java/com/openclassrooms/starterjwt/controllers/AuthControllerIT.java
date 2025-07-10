d47de95dc8db0eedc5abc67f94de6b2c6e40da7b
Hugo-Robert-1 <robert.hugo.051@gmail.com>
2025-07-10 11:33:01 +0200

    tests(AuthControllerIT): ajout de test d'intégration pour la connexion et pour l'inscription

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerIT.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.request.SignupRequest;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 
@@ -76,5 +77,55 @@ class AuthControllerIT {
 				.content(objectMapper.writeValueAsString(request)))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.message").value("User registered successfully!"));
+	}
+
+	@Test
+	void authenticateUser_shouldReturn200_whenLoginIsValid() throws Exception {
+		SignupRequest request = new SignupRequest();
+		request.setEmail("john.doe@email.com");
+		request.setFirstName("John");
+		request.setLastName("Doe");
+		request.setPassword("azerty51");
+
+		mockMvc.perform(post("/api/auth/register")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.message").value("User registered successfully!"));
+
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setEmail("john.doe@email.com");
+		loginRequest.setPassword("azerty51");
+
+		mockMvc.perform(post("/api/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginRequest)))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void registerUser_shouldReturnErrorIfMissingValue() throws Exception {
+		SignupRequest signUpRequest = new SignupRequest();
+		signUpRequest.setEmail("user@email.com");
+		signUpRequest.setFirstName("User");
+		signUpRequest.setLastName(null);
+		signUpRequest.setPassword("password");
+
+		mockMvc.perform(post("/api/auth/register")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(signUpRequest)))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void authenticateUser_shouldReturnErrorIfMissingValue() throws Exception {
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setEmail("user@email.com");
+		loginRequest.setPassword(null);
+
+		mockMvc.perform(post("/api/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginRequest)))
+				.andExpect(status().isBadRequest());
 	}
 }
